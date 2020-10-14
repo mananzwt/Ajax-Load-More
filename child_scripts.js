47a17ec5c71a90dbf06f21af4797ed879106ccaa
mananzwt <71812880+mananzwt@users.noreply.github.com>
2020-10-14 18:59:24 +0530

    Update child_scripts.js

--- child_scripts.js
+++ child_scripts.js
@@ -67,6 +67,57 @@ $(document).ready(function() {
     });
     
     
+    // javascript version of the ajax
+    // pagination numbers click event
+    $(document).on( 'click', '.pagination a', function( event ) {
+    
+        //event.preventDefault();
+        var paged 			= $(this).attr('data-id');
+        var posts_per_page  = jQuery(document).find('#posts_per_page').val();
+
+	    var xhttp = new XMLHttpRequest();
+
+	    xhttp.open("POST", myChildAjax.ajaxurl, true);
+	    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+
+	    xhttp.onload = function () {
+
+	        if (this.status >= 200 && this.status < 400) {
+
+	            let response = JSON.parse(this.responseText);
+	    		document.getElementById("ajax-posts").innerHTML = response.message;
+
+	    		var next_page_id = parseInt(paged) + 1;
+                if(paged > 0){
+                    var prev_page_id = parseInt(paged) - 1;
+                }else{
+                    var prev_page_id = 0;
+                }
+
+                //jQuery("#prev-post").attr('data-id',prev_page_id);
+                if(document.getElementsByClassName("prev-post")[0] != undefined){
+                	document.getElementsByClassName("prev-post")[0].setAttribute("data-id", prev_page_id);	
+                }
+                
+                //jQuery("#next-post").attr('data-id',next_page_id);
+                if(document.getElementsByClassName("next-post")[0] != undefined){
+	                document.getElementsByClassName("next-post")[0].setAttribute("data-id", next_page_id);
+            	}
+
+
+	        } else {
+	            // If fail
+	            console.log(this.response);
+	        }
+	    };
+	    xhttp.onerror = function() {
+	        // Connection error
+	    };
+
+	    // send the data with httprequest
+    	xhttp.send('_ajax_nonce='+myChildAjax.ajax_nonce+'&action=more_post_ajax&paged='+paged+'&posts_per_page='+posts_per_page);
+    
+	});
     
     
 });
